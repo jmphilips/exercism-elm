c4380d5611c602ecba91cd63f4258eb4d344ed04
Joshua Philips <jmichaelphilips@gmail.com>
2017-06-11 10:04:06 -0500

    Refactored Bob.

--- bob/Bob.elm
+++ bob/Bob.elm
@@ -1,60 +1,36 @@
 module Bob exposing (..)
 import Char exposing (isDigit)
+import String exposing (toLower, toUpper, endsWith, trim, isEmpty)
 
+
+type Phrase =
+  Yell | Question | Silence | Other
+
+
+hey : String -> String
 hey str =
-  let
-    response =
-      if (isQuestion str) && (onlyNumbers str) then
-        "Sure."
-      else if (isShouted str) && (isQuestion str) then
-        "Whoa, chill out!"
-      else if (isQuestion str) then
-        "Sure."
-      else if (isSilent str) then
-        "Fine. Be that way!"
-      else if (onlyNumbers str) then
-        "Whatever."
-      else if (isShouted str) then
-        "Whoa, chill out!"
-      else
-        "Whatever."
-  in
-    response
+  case toPhrase str of
+    Yell -> "Whoa, chill out!"
+    Question -> "Sure."
+    Silence -> "Fine. Be that way!"
+    Other -> "Whatever."
 
-deleteEmpties : String -> String
-deleteEmpties str =
-  str
-  |> String.split "\n"
-  |> String.join ""
-  |> String.split "\t"
-  |> String.join ""
-  |> String.split " "
-  |> String.join ""
+toPhrase : String -> Phrase
+toPhrase str =
+  if isShouted str then Yell
+  else if isQuestion str then Question
+  else if isSilent str then Silence
+  else Other
 
-deletePunct : String -> String
-deletePunct str =
-  str
-  |> String.split ", "
-  |> String.join ""
-  |> String.split "."
-  |> String.join ""
-  |> String.split "'"
-  |> String.join ""
-  |> String.split "?"
-  |> String.join ""
-
-isSilent : String -> Bool
-isSilent str =
-  (deleteEmpties str) == ""
 
 isShouted : String -> Bool
 isShouted str =
-  String.toUpper str == str
+  (toUpper str == str) && not (toLower str == str)
 
 isQuestion : String -> Bool
 isQuestion str =
-   List.head (List.reverse (String.split "" str)) == Just "?"
+  endsWith "?" str
 
--- includesLetters : String -> Bool
-onlyNumbers str =
-  String.all isDigit (deletePunct str)
+isSilent : String -> Bool
+isSilent str =
+  str |> trim |> isEmpty
